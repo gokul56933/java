dec8288b443a1b7aadcebd1aecd4fc3832307850
gokul56933 <gokulloganathan56933@gmail.com>
2025-05-18 00:38:24 +0530

    feat : printed helloworld

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class helloworld {
+
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("This is a simple Java program.");
+}
+}
