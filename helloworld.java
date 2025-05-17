362f153e58cd7423ed9a87a24f58ef797d9be3cc
Gokul <gokulloganathan56933@gmail.com>
2025-05-18 00:45:30 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -4,5 +4,6 @@ public class helloworld {
 public static void main(String[] args) {
     System.out.println("Hello, World!");
     System.out.println("This is a simple Java program.");
+    System.out.println("gokul.");
 }
 }
